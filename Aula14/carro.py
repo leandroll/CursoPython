e4236a1d62a39c580feebbefba971d8a07ced8d8
Leandro Lemos Lima <leandro.freebsd@gmail.com.br>
2024-04-13 22:53:55 -0300

    correção da aula 14

--- Aula14/carro.py
+++ Aula14/carro.py
@@ -1,6 +1,6 @@
 class Carro:    
-    def __init__(self,marca,modelo,cor,combustivel) -> None:
-        pass
+    def __init__(self,marca,modelo,cor,combustivel):
+        
         self.marca = marca
         self.modelo = modelo
         self.cor = cor
@@ -9,30 +9,27 @@ class Carro:
         self.ligado = False
         self.velocidade = 0
 
-        def ligar(self):
-            if self.ligado:
-                print("carro já está ligado, nada acontece")
-            else:
-                print(f"{self.modelo} ligado")
-                self.ligado = True
-
-        def desligar(self):
-            if self.ligado:
-                print(f"{self.modelo} desligado")
-                self.ligado = False
-            else:
-                print("carro já está ligado, nada acontece")
-
-        def acelerar(self):
-            if self.ligado:
-                self.velocidade += 1
-                print(f"{self.velocidade}Km/h.")
-            else:
-                print("Não é possível acelerar, carro desligar")
-
-        def frear(self):
-            if self.ligado:
-                self.velocidade -= 1
-                print(f"{self.velocidade}Km/h.")
-            else:
-                print("Não é possível frear, carro desligar")
+    def ligar(self):
+        if self.ligado:
+            print("carro já está ligado, nada acontece")
+        else:
+            print(f"{self.modelo} ligado")
+            self.ligado = True
+    def desligar(self):
+        if self.ligado:
+            print(f"{self.modelo} desligado")
+            self.ligado = False
+        else:
+            print("carro já está ligado, nada acontece")
+    def acelerar(self):
+        if self.ligado:
+            self.velocidade += 1
+            print(f"{self.velocidade}Km/h.")
+        else:
+            print("Não é possível acelerar, carro desligar")
+    def frear(self):
+        if self.ligado:
+            self.velocidade -= 1
+            print(f"{self.velocidade}Km/h.")
+        else:
+            print("Não é possível frear, carro desligar")
